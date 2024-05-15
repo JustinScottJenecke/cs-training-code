8fcb54f13151cc9c857e73dfac0624770bf9c135
JustinScottJenecke <justinjenecke@gmail.com>
2024-05-15 14:44:23 +0200

    adds swop method to main

---
+++ essential-training-2/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5, y = 6;
+
+            Console.WriteLine($"X is {x} and Y is {y} before swapping.");
+
+            Swap(x, y);
+
+            Console.WriteLine($"X is {x} and Y is {y} after swapping.");
+
+            static void Swap(object a, object b) {
+                object temp = a;
+                a = b;
+                b = temp;
+            }
+        }
+    }
+}
