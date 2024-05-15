c35d91471e1db15e2fa1bc9d90224de2555fff3e
JustinScottJenecke <justinjenecke@gmail.com>
2024-05-15 15:29:55 +0200

    tries swapping but fails due to use of generic object in Swap method

--- essential-training-2/ConsoleApp1/Program.cs
+++ essential-training-2/ConsoleApp1/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace ConsoleApp1
+﻿using ConsoleApp1.Library;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            // ----- Value Types -----
+
+            var p1 = new Person
+            {
+                FirstName = "John",
+                LastName = "Smith",
+                Age = 22
+            };
+
+            var p2 = new Person
+            {
+                FirstName = "Jane",
+                LastName = "Snow",
+                Age = 32
+            };
+
+            Console.WriteLine($"P1 is {p1.FirstName} and P2 is {p2.FirstName} before swapping.");
+
+            Swap(p1.FirstName, p2.FirstName);
+
+            Console.WriteLine($"P1 is {p1.FirstName} and P2 is {p2.FirstName} after swapping.");
+
+            // ----- Value Types -----
+
             int x = 5, y = 6;
 
             Console.WriteLine($"X is {x} and Y is {y} before swapping.");
